e22b19b394a4c826b62d74ec43f7b81a90c42c6c
Glitch (hello-express) <none>
2020-06-26 06:08:19 +0000

    ⛱🍼 Checkpoint ./utils/web-craw.js:5637465/4460

--- utils/web-craw.js
+++ utils/web-craw.js
@@ -33,6 +33,7 @@ exports.getContent = (url) => {
 
 exports.getFavicon = (dom) => {
   let linkTag = dom.split("<link");
+  let favicon = null;
   
   for (let tag of linkTag) {
     let domTag = tag.split(">")[0];
@@ -40,28 +41,47 @@ exports.getFavicon = (dom) => {
     if (sanitizedTag.includes("rel=icon") || sanitizedTag.includes("rel=shortcuticon")) {
       let obj = domTag.split("href=")[1];
       let char = obj[0];
+      
+      let url = obj.split(char)[1];
+      favicon = url;
     }
   }
   
+  let metaTag = dom.split("<meta");
+  for (let tag of metaTag) {
+    let domTag = tag.split(">")[0];
+    let sanitizedTag = domTag.replace(/ /gi).replace(/\"/gi, "").replace(/\'/gi, "").replace(/\`/gi, "");
+    console.log(sanitizedTag);
+    if (sanitizedTag.includes("itemprop=image")) {
+      let obj = domTag.split("content=")[1];
+      let char = obj[0];
+          
+      let url = obj.split(char)[1];
+      favicon = url;
+    }
+  }
+  
+  /*
   let favicon = null;
   let nodeList = dom.getElementsByTagName("link");
   for (var i = 0; i < nodeList.length; i++) {
     if ((nodeList[i].getAttribute("rel") == "icon")||(nodeList[i].getAttribute("rel") == "shortcut icon")) {
       favicon = nodeList[i].getAttribute("href");
     }
-  }
+  }*/
   
   return favicon;
 }
 
 exports.getDescription = (dom) => {
   let desc = null;
+  /*
   let nodeList = dom.getElementsByTagName("meta");
   for (var i = 0; i < nodeList.length; i++) {
     if ((nodeList[i].getAttribute("name") == "description") && (nodeList[i].getAttribute("content") != null)) {
       desc= nodeList[i].getAttribute("content");
     }
-  }
+  }*/
   
   return desc;
 }
@@ -70,9 +90,18 @@ exports.getData = async (url) => {
   return new Promise(async (resolve, reject) => {
     
     let dom = await this.getContent(url);
+    // console.log(dom)
     
     let favicon = this.getFavicon(dom);
     let description = this.getDescription(dom);
+    
+    if (!url.includes("://")) {
+      url =?
+    }
+    
+    if (!favicon.includes("://")) {
+      if (!favicon.includes(url.split()))
+    }
     
     resolve({favicon, description})
     
