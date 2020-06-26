bbf53a5e8e28fbd3188cda36902650e5327aa0e4
Glitch (hello-express) <none>
2020-06-26 01:26:58 +0000

    🎅🚣 Checkpoint ./views/index.ejs:5637465/115 ./routes/apiSearch.js:5637465/431 ./views/search.ejs:5637465/37 ./utils/searchGoogle.js:5637465/2910

--- routes/apiSearch.js
+++ routes/apiSearch.js
@@ -1,13 +1,23 @@
 const search_engine = require('search-engine-nodejs').default;
+const searchGoogle = require('../utils/searchGoogle');
 
 const express = require("express");
 const Router = new express.Router();
 
 Router.get("/api/search/:search_query", (req, res) =>  {
   const search_query = req.params.search_query;
+  searchGoogle(search_query)
+            .then(results => {
+                //Returns a 200 Status OK with Results JSON back to the client.
+                res.status(200);
+                console.log(results);
+                res.json(results);
+            });
+  /*
   search(search_query, (results) => {
     res.json(results);
   })
+  */
 });
 
 function search (search_query, callback) {
