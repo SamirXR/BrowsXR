427bc664c2c04ab0c7214a95bcac585f67a3529c
Glitch (hello-express) <none>
2020-06-26 01:50:01 +0000

    ⚡️🗻 Checkpoint ./views/index.ejs:5637465/302 ./public/css/app.css:5637465/558 ./views/search.ejs:5637465/1099 ./public/js/app.js:5637465/1309

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,33 @@ function search() {
   let query = document.getElementById("search-input").value;
   window.location.href = "/search/" + query;
 }
+
+function appendResult (title, description, link) {
+  const dom = document.getElementById("results");
+  dom.innerHTML = dom.innerHTML + `
+    <div>
+      <h2 href="${link}">${title}</h2>
+      <a href="${link}">${link}</a>
+      <p>${description}</p>
+    </div>
+`;
+}
+      
+function initSearch () {
+  var callback = (response) => {
+    response = JSON.parse(response);
+    for (let result of response) {
+      appendResult(result.title, result.description, result.link)
+    }
+  };
+      
+  var http = new XMLHttpRequest();
+  http.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      callback(this.responseText);
+    }
+  }
+      
+  http.open("GET", "/api/search/<%=search_query%>");
+  http.send();
+}
