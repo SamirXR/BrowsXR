148b591797d1dc3b74de8b3120ed5cb29e2d2f1b
Glitch (hello-express) <none>
2022-03-18 09:29:24 +0000

    🌷🎄 Checkpoint ./README.md:24332185/390 ./.glitch-assets:24332185/484 ./utils/proxy-craw.js:24332185/8 ./engine/index.js:24332185/1968 ./engine/database.json:24332185/809 ./views/search.ejs:24332185/28 ./views/index.ejs:24332185/16

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # BrowsXR
 
+ A Basic search engine :)
+
+We offers a 100% transparent privacy polic :)
+
+- Fast Search
+- Identifies protected and unprtected sites
+- With Built in Proxy
+
+However,Proxy isnt functional for now,but we'll fix it ASAP :)
+
+
--- engine/index.js
+++ engine/index.js
@@ -4,17 +4,16 @@ exports.search = (str) => {
   while (str.includes("  ")) {
     str = str.replace("  ", " ");
   }
-  
-  let temp_results = []
-  
+
+  let temp_results = [];
+
   let words = str.split(" ");
-  
+
   for (let entry of entries) {
     let points = 0;
-    
+
     for (let entry_word of entry.keywords) {
-      if (str.includes(entry_word))
-        points++;
+      if (str.includes(entry_word)) points++;
     }
 
     if (points >= 1) {
@@ -37,16 +36,15 @@ exports.search = (str) => {
     }
   } else {
     return orderList(temp_results);
-  }  
-}
+  }
+};
 
-function orderList (list) {
+function orderList(list) {
   let temp_list = [];
 
   for (let i = 10; i >= 1; i--) {
     for (let item of list) {
-      if (item.points == i)
-        temp_list.push(item);
+      if (item.points == i) temp_list.push(item);
     }
   }
 
